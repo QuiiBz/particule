3c1c9bd76189063df9aa1c440f22604b0f139703
QuiiBz <tom.lienrd@gmail.com>
2021-11-06 23:03:44 +0100

    chore: use custom build script

---
+++ build.js
@@ -0,0 +1,44 @@
+const esbuild = require('esbuild');
+const args = process.argv.slice(2)
+
+const watch = args.includes('--watch')
+
+const formats = ['cjs', 'esm', 'iife'];
+const entrypoints = [
+  ['./src/index.ts', './dist/index'],
+  ['./src/helpers/localStorage.ts', './dist/helpers/localStorage'],
+  ['./src/helpers/reset.ts', './dist/helpers/reset']
+]
+
+const build = async () => {
+  for (format of formats) {
+    console.log(`┌ Building ${format}...`)
+
+    for (entrypoint of entrypoints) {
+      const [entry, out] = entrypoint
+      const outfile = `${out}.${format}.js`
+      const icon = entrypoint === entrypoints[entrypoints.length - 1] ? '└' : '├'
+
+      try {
+        await esbuild.build({
+          entryPoints: [entry],
+          outfile,
+          external: ['react'],
+          format,
+          minify: true,
+          bundle: true,
+          watch,
+        })
+
+        console.log(`${icon} ${entry} -> ${outfile}`)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    console.log('')
+  }
+}
+
+build()
+
