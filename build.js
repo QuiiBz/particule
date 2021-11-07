76d57489ae36ce0f01bbacc016925fd30a58c40d
QuiiBz <tom.lienrd@gmail.com>
2021-11-07 10:15:03 +0100

    feat: use estrella for build

--- build.js
+++ build.js
@@ -1,45 +1,21 @@
+#!/usr/bin/env node
+
 /* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-console */
-const esbuild = require('esbuild');
+/* eslint-disable guard-for-in */
 
-const args = process.argv.slice(2);
-const watch = args.includes('--watch');
+const { build, glob } = require('estrella');
 
-const formats = ['cjs', 'esm', 'iife'];
-const entrypoints = [
-  ['./src/index.ts', './dist/index'],
-  ['./src/helpers/localStorage.ts', './dist/helpers/localStorage'],
-  ['./src/helpers/reset.ts', './dist/helpers/reset'],
-];
+const formats = ['esm', 'cjs'];
 
-const build = async () => {
+const run = async () => {
   for (const format of formats) {
-    console.log(`┌ Building ${format}...`);
-
-    for (const entrypoint of entrypoints) {
-      const [entry, out] = entrypoint;
-      const outfile = `${out}.${format}.js`;
-      const icon = entrypoint === entrypoints[entrypoints.length - 1] ? '└' : '├';
-
-      try {
-        await esbuild.build({
-          entryPoints: [entry],
-          outfile,
-          external: ['react'],
-          format,
-          minify: true,
-          bundle: true,
-          watch,
-        });
-
-        console.log(`${icon} ${entry} -> ${outfile}`);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    console.log('');
+    await build({
+      entry: glob('./src/**/*.ts'),
+      outdir: './dist/esm',
+      format,
+      tslint: false,
+    });
   }
 };
 
-build();
+run();
---
+++ src/helpers/index.ts
@@ -0,0 +1,2 @@
+export { default as localStorageAtom } from './localStorage';
+export * from './reset';
