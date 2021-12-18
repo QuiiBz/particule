66ca789deddc48a50a6681c84f192538a3add759
QuiiBz <tom.lienrd@gmail.com>
2021-12-18 11:36:57 +0100

    feat(site): add plausible analytics

---
+++ site/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script defer data-domain="particule.vercel.app" src="https://plausible.lnrd.fr:20443/js/plausible.js" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
