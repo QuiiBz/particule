389b2e75b2b61b3e7fc65b2a787ab707df325eb0
QuiiBz <tom.lienrd@gmail.com>
2021-12-18 10:45:21 +0100

    fix(core): localStorage atom only client-side

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,16 +1,20 @@
 import { createAtom } from '../atoms';
 
+const isClient = typeof window !== 'undefined';
+
 const localStorageAtom = (key: string) =>
   createAtom({
     beforeValueSet: (_, value, firstSet) => {
-      if (firstSet && localStorage.getItem(key) !== null) {
+      if (firstSet && isClient && localStorage.getItem(key) !== null) {
         return JSON.parse(localStorage.getItem(key)!);
       }
 
       return value;
     },
     afterValueSet: (_, value) => {
-      localStorage.setItem(key, JSON.stringify(value));
+      if (isClient) {
+        localStorage.setItem(key, JSON.stringify(value));
+      }
     },
   });
 
